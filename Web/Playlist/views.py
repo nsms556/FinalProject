ffbc8065db8f18d543a8cbb39559616b8e5411f3
spongebob03 <bmofinnjake08@gmail.com>
2021-08-27 17:18:34 +0900

    [Backend] Change response format to json

--- Web/Playlist/views.py
+++ Web/Playlist/views.py
@@ -8,15 +8,15 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def index(request):
     # json으로 나오는 추천 리스트 정보 
+    # 추론 돌리고 나온 results_210812-170628_test.json 사용
+    # (현재 확인 편의상 simple.json 사용)
     json_path = os.path.join(settings.BASE_DIR, 'Playlist/results/simple.json')
     with open(json_path, encoding='utf-8') as json_file:
         json_data = json.load(json_file)
-        playlists = []
-        for i in range(len(json_data)):
-            playlists.append(json_data[i])
 
-    return render(request, 'index.html', {'playlists':playlists})
+    return JsonResponse({'playlists':json_data}, json_dumps_params={'ensure_ascii':True})
 
+# [/detail] 플레이리스트 내부 곡 정보
 def detail(request):
     conn = sqlite3.connect('data.db')
     cur = conn.cursor()
@@ -24,11 +24,9 @@ def detail(request):
     json_path = os.path.join(settings.BASE_DIR, 'Playlist/results/simple.json')
     with open(json_path, encoding='utf-8') as json_file:
         json_data = json.load(json_file)
-        playlists = []
         playlist_songs = []
 
         for i in range(len(json_data)):
-            playlists.append(json_data[i])
             songs = []
             for song in json_data[i]['songs']:
                 cur.execute(f'select song_name, artist_name_basket from song_meta where id={song}')
@@ -37,4 +35,4 @@ def detail(request):
                     songs.append(row)
             playlist_songs.append(songs)
 
-    return JsonResponse({'playlists': playlists, 'playlist_songs':playlist_songs}, json_dumps_params={'ensure_ascii':True})
+    return JsonResponse({'playlist_songs':playlist_songs}, json_dumps_params={'ensure_ascii':True})
