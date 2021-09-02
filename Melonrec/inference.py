eae760e943b66e9f6b875450be5aa1e8d22f59d9
nsms556 <nsms556@kakao.com>
2021-09-02 19:20:03 +0900

    Update [Modeling] inference.py

--- Melonrec/inference.py
+++ Melonrec/inference.py
@@ -1,4 +1,3 @@
-from torch._C import device
 from tqdm import tqdm
 
 import numpy as np
@@ -21,7 +20,7 @@ class Recommender(nn.Module) :
         super(Recommender, self).__init__()
 
         self.autoencoder = self._load_autoencoder(autoencoder_model_path)
-        self.vectorizer = self._load_vectorizer(vectorizer_model_path)
+        self.vectorizer, self.word_dict = self._load_vectorizer(vectorizer_model_path)
         self.tokenizer = KhaiiiApi()
         self.cos = nn.CosineSimilarity(dim=1)
 
@@ -38,7 +37,7 @@ class Recommender(nn.Module) :
         vectors = Vectors(name=model_path)
         embedding = nn.Embedding.from_pretrained(vectors.vectors, freeze=False)
 
-        return embedding
+        return embedding, pd.Series(vectors.stoi)
 
     def autoencoder_embedding(self, question_loader:DataLoader, genre:bool) :
         with torch.no_grad() :
@@ -64,5 +63,69 @@ class Recommender(nn.Module) :
 
                     return auto_emb
 
-    def word2vec_embedding(self, question_loader) :
-        
+    def word2vec_embedding(self, question_data:pd.DataFrame) :
+        def find_word_embed(words) :
+            ret = []
+            try :
+                for word in words :
+                    ret.append(self.word_dict[word])
+            except KeyError :
+                pass
+  
+            return ret
+
+        w2v_emb = self.pre_w2v_emb.copy()
+
+        p_ids = question_data['id']
+        p_titles = question_data['plylst_title']
+        p_tags = question_data['tags']
+        p_dates = question_data['updt_date'].str[:7].str.split('-')
+
+        question_data['tokens'] = p_titles + p_tags + p_dates
+        question_data['emb_input'] = question_data['tokens'].map(lambda x : find_word_embed(x))
+
+        outputs = []
+        for e in question_data['emb_input'] :
+            word_output = self.vectorizer(torch.LongTensor(e))
+            if len(word_output) :
+                output = torch.mean(word_output, axis=0)
+            else :
+                output = torch.zeros(200)
+        outputs.append(output)
+        outputs = torch.stack(outputs)
+
+        w2v_emb = pd.concat([w2v_emb, pd.DataFrame(outputs.tolist(), index=p_ids)])
+
+        return w2v_emb
+
+        '''
+            p_words = []
+            for q in tqdm(question_data) :
+                p_id = q['id']
+                p_title = q['plylst_title']
+                p_tags = q['tags']
+                p_date = q['updt_date'][:7].split('-')
+
+                p_title_tokens = self.tokenizer.analyze([p_title])
+                if len(p_title_tokens):
+                    p_title_tokens = p_title_tokens[0]
+                else:
+                    p_title_tokens = []
+
+                p_words = p_title_tokens + p_tags + p_date
+
+                word_input = np.array([])
+                for p_word in p_words :
+                    try :
+                        x_word = np.append(word_input, self.word_dict[p_word])
+                    except :
+                        pass
+                word_input = torch.from_numpy(x_word)
+
+                word_output = self.vectorizer(word_input)
+
+                if len(word_output) :
+                    output = torch.mean(word_output, axis=0)
+                else :
+                    output = torch.zeros(200)
+        '''
