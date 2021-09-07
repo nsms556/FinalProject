67ddca61a3f88edb4fe59615951b918fb12fd973
devmei <devmei.n@gmail.com>
2021-09-07 22:34:44 +0900

    Update [Front-end] user_reducer.js

--- Web/client/src/_reducers/user_reducer.js
+++ Web/client/src/_reducers/user_reducer.js
@@ -1,6 +1,7 @@
 import {AUTH_USER, LOGIN_USER, LOGOUT_USER, REGISTER_USER} from '../_actions/types';
 
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = {}, action) {
 
     switch (action.type) {
