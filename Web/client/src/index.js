4ec7c47190c782578bc0d1b574a63057a7e0dff1
devmei <devmei.n@gmail.com>
2021-09-07 21:46:18 +0900

    Update [Front-end] index.js

--- Web/client/src/index.js
+++ Web/client/src/index.js
@@ -20,6 +20,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
 import "assets/plugins/nucleo/css/nucleo.css";
 import "assets/scss/argon-dashboard-react.scss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -27,14 +32,27 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
 
+import Reducer from './_reducers';
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route path="/admin" render={(props) => <AdminLayout {...props} />}/>
-            <Route path="/auth" render={(props) => <AuthLayout {...props} />}/>
-            <Redirect from="/" to="/admin/index"/>
-        </Switch>
-    </BrowserRouter>,
+    <Provider
+        store={createStoreWithMiddleware(
+            Reducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+    >
+        <BrowserRouter>
+            <Switch>
+                <Route path="/admin" render={(props) => <AdminLayout {...props} />}/>
+                <Route path="/auth" render={(props) => <AuthLayout {...props} />}/>
+                <Redirect from="/" to="/admin/index"/>
+            </Switch>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById("root")
 );
