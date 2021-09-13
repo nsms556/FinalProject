9bb95dfe22e24008d7f071f18bc314154b1e92c1
devmei <devmei.n@gmail.com>
2021-09-14 04:04:08 +0900

    Update [Front-end] PlaylistPage.js

--- Web/client/src/views/PlaylistPage/PlaylistPage.js
+++ Web/client/src/views/PlaylistPage/PlaylistPage.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Card, CardHeader, Container, Row} from "reactstrap";
 
-import Header from "../../components/Headers/Header";
+import axios from "axios";
 
-import SongInfo from "./Sections/SongInfo";
+import Header from "../../components/Headers/Header";
+import SongInfo from "../../components/SongInfo";
+
+import MakePlaylist from "./Sections/MakePlaylist";
 
 
 function PlaylistPage() {
+
+    const [SongList, setSongList] = useState(null);
+
+    useEffect(() => {
+
+        axios.get('http://127.0.0.1:8000/playlist/recommend')
+            .then(response => {
+                if (response.data) {
+                    setSongList(response.data.song_list);
+                } else {
+                    alert('Playlist(song_list)를 가져오지 못했습니다.');
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, [SongList]);
+
     return (
         <>
             <Header/>
@@ -14,17 +35,26 @@ function PlaylistPage() {
                 <Row>
                     <div className="col">
                         <Card className="shadow">
-                            <CardHeader className="border-0">
-                                <h3 className="mb-0">My Playlist</h3>
-                            </CardHeader>
-                            {/* Song Info */}
-                            <SongInfo/>
+                            {
+                                SongList ?
+                                    (
+                                        <>
+                                            <CardHeader className="border-0">
+                                                <h3 className="mb-0">My Playlist</h3>
+                                            </CardHeader>
+                                            <SongInfo song_list={SongList}/>
+                                        </>
+                                    ) :
+                                    (
+                                        <MakePlaylist/>
+                                    )
+                            }
                         </Card>
                     </div>
                 </Row>
             </Container>
         </>
     );
-};
+}
 
 export default PlaylistPage;
