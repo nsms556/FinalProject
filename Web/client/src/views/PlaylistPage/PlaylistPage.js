800abaa7e2b6142e0c3db459ba7ddfca862cb572
devmei <devmei.n@gmail.com>
2021-09-15 15:24:34 +0900

    Update [Front-end] PlaylistPage.js

--- Web/client/src/views/PlaylistPage/PlaylistPage.js
+++ Web/client/src/views/PlaylistPage/PlaylistPage.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from "react";
 import {Card, CardHeader, Container, Row} from "reactstrap";
+import {trackPromise} from 'react-promise-tracker';
 
 import axios from "axios";
 
 import Header from "components/Headers/Header.js";
+import LoadingIndicator from "components/LoadingIndicator";
 import SongInfo from "components/SongInfo.js";
 
 import MakePlaylist from "./Sections/MakePlaylist";
@@ -12,20 +14,24 @@ import MakePlaylist from "./Sections/MakePlaylist";
 function PlaylistPage() {
 
     const [SongList, setSongList] = useState(null);
+    const [ViewPage, setViewPage] = useState(false);
 
     useEffect(() => {
-
-        axios.get('http://127.0.0.1:8000/playlist/recommend')
-            .then(response => {
-                if (response.data) {
-                    setSongList(response.data.song_list);
-                } else {
-                    alert('Playlist(song_list)를 가져오지 못했습니다.');
-                }
-            })
-            .catch(error => {
-                console.log(error);
-            })
+        trackPromise(
+            axios.get('http://127.0.0.1:8000/playlist/songs')
+                .then(response => {
+                    if (response.data) {
+                        setSongList(response.data.song_list);
+                    } else {
+                        alert('Playlist(song_list)를 가져오지 못했습니다.');
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        ).then(_ => {
+            setViewPage(true);
+        })
     }, [SongList]);
 
     return (
@@ -34,22 +40,26 @@ function PlaylistPage() {
             <Container className="mt--7" fluid>
                 <Row>
                     <div className="col">
-                        <Card className="shadow">
-                            {
-                                SongList ?
-                                    (
-                                        <>
-                                            <CardHeader className="border-0">
-                                                <h3 className="mb-0">My Playlist</h3>
-                                            </CardHeader>
-                                            <SongInfo song_list={SongList}/>
-                                        </>
-                                    ) :
-                                    (
-                                        <MakePlaylist/>
-                                    )
-                            }
-                        </Card>
+                        <LoadingIndicator/>
+                        {
+                            ViewPage &&
+                            <Card className="shadow">
+                                {
+                                    SongList ?
+                                        (
+                                            <>
+                                                <CardHeader className="border-0">
+                                                    <h3 className="mb-0">My Playlist</h3>
+                                                </CardHeader>
+                                                <SongInfo song_list={SongList}/>
+                                            </>
+                                        ) :
+                                        (
+                                            <MakePlaylist/>
+                                        )
+                                }
+                            </Card>
+                        }
                     </div>
                 </Row>
             </Container>
