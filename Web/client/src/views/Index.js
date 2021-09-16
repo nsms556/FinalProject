aaef52426a47f49e08566fbd5de27930aae590f9
MEI Y <44670842+devmei@users.noreply.github.com>
2021-09-16 20:00:24 +0900

    Update [Front-end] Index.js

--- Web/client/src/views/Index.js
+++ Web/client/src/views/Index.js
@@ -19,7 +19,9 @@
 import React from "react";
 
 // reactstrap components
-import {Card, CardHeader, Container} from "reactstrap";
+import {Card, CardBody, CardImg, CardText, CardTitle, Col, Container, Row} from "reactstrap";
+
+import {GithubOutlined, HomeFilled} from "@ant-design/icons";
 
 import Header from "components/Headers/Header.js";
 
@@ -30,14 +32,97 @@ const Index = () => {
         <>
             <Header/>
             <Container className="mt--7" fluid>
-                <Card className="shadow">
-                    <CardHeader className="border-0 text-center">
+                <div style={{marginLeft: "auto", marginRight: "auto", width: "35%"}}>
+                    <Card className="shadow" style={{padding: "1rem"}}>
                         <img alt="programmers" src="/images/mussg.gif"/>
-                    </CardHeader>
-                </Card>
+                    </Card>
+                </div>
+                <br/>
+                <Row>
+                    <Col>
+                        <Card style={{width: "18rem"}}>
+                            <CardImg
+                                alt="S"
+                                src="/images/11ML.png"
+                                top
+                            />
+                            <CardBody>
+                                <CardTitle><strong>팀장</strong> S.</CardTitle>
+                                <CardText>
+                                    프로젝트 모델 개발 및 학습<br/>
+                                    모델 성능 향상
+                                </CardText>
+                                <a href="https://github.com/AidevB6/FinalProject" target="_blank" rel="noreferrer">
+                                    <HomeFilled/>
+                                </a>
+                                <span>　</span>
+                                <a href="https://github.com/nsms556" target="_blank" rel="noreferrer">
+                                    <GithubOutlined/>
+                                </a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{width: "18rem"}}>
+                            <CardImg
+                                alt="L"
+                                src="/images/15.png"
+                                top
+                            />
+                            <CardBody>
+                                <CardTitle>팀원 L.</CardTitle>
+                                <CardText>
+                                    데이터 전처리 및 모델 개발<br/>
+                                    프로젝트 PPT 작성 및 발표
+                                </CardText>
+                                <a href="https://github.com/lymchgmk" target="_blank" rel="noreferrer">
+                                    <GithubOutlined/>
+                                </a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{width: "18rem"}}>
+                            <CardImg
+                                alt="B"
+                                src="/images/27.png"
+                                top
+                            />
+                            <CardBody>
+                                <CardTitle>팀원 B.</CardTitle>
+                                <CardText>
+                                    Django framework 기반 API 개발<br/>
+                                    DB 구축 및 AWS 서버 배포
+                                </CardText>
+                                <a href="https://github.com/spongebob03" target="_blank" rel="noreferrer">
+                                    <GithubOutlined/>
+                                </a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{width: "18rem"}}>
+                            <CardImg
+                                alt="Y"
+                                src="/images/32.png"
+                                top
+                            />
+                            <CardBody>
+                                <CardTitle>팀원 Y.</CardTitle>
+                                <CardText>
+                                    웹 개발(FE)<br/>
+                                    React 기반 웹 페이지 구축
+                                </CardText>
+                                <a href="https://github.com/devmei" target="_blank" rel="noreferrer">
+                                    <GithubOutlined/>
+                                </a>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row><br/>
             </Container>
         </>
     );
-};
+}
 
 export default Index;
