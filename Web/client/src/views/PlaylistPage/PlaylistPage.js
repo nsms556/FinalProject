d39f561271505ab5cac1dcd5d70f2581a82254e1
devmei <devmei.n@gmail.com>
2021-09-16 15:44:57 +0900

    Update [Front-end] PlaylistPage.js

--- Web/client/src/views/PlaylistPage/PlaylistPage.js
+++ Web/client/src/views/PlaylistPage/PlaylistPage.js
@@ -10,6 +10,11 @@ import SongInfo from "components/SongInfo.js";
 import MakePlaylist from "./Sections/MakePlaylist";
 
 
+axios.defaults.withCredentials = true;
+
+
+const host = '127.0.0.1'
+
 function PlaylistPage() {
 
     const [SongList, setSongList] = useState(null);
@@ -20,7 +25,7 @@ function PlaylistPage() {
         setViewPage(false);
 
         trackPromise(
-            axios.get('http://127.0.0.1:8000/playlist/songs')
+            axios.get(`http://${host}:8000/playlist/songs`)
                 .then(response => {
                     if (response.data) {
                         setSongList(response.data.song_list);
